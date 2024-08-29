11e40defaacf3f6a21fb96d4699aad11db983b6c
Guilherme Morais Banhos <guilme923@gmail.com>
2024-08-29 14:40:29 +0000

    aplicação que somo dois numeros inteiros

--- OlaMundo.java
+++ OlaMundo.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class OlaMundo {
-    public static void main(String args[]) {
-        System.out.printin("Olá, Mundo!!! :D");
-    
+    public static void main (String args[]) {
+        Scanner teclado = new Scanner(System.in);
+        double num1, num2;
+        double result;
+        System.out.println("informe o primeiro numero: ");
+        num1 = teclado.nextInt();
+        System.out.println ("informe o segundo numero ");
+        num2 = teclado.nextInt();
+        result = num1 / num2;
+
+        System.out.println("resultado = " + result);
+    System.exit(0);
     }
          
 
